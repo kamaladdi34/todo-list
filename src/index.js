b6fc977a834465b3a1945c3580850768e07be7ae
monmac <monmac@MacBook-Pro-de-monmac.local>
2023-09-08 20:16:31 +0100

    Create index.html structure

--- src/index.js
+++ src/index.js
@@ -2,3 +2,7 @@ import manager from "./todo-list-manager";
 let listId = manager.createList('MyList','My nice big list',[],false);
 console.log(manager.getList(listId));
 manager.getList(listId).addTodo('Feed Dog', 'Feed dog some food', 'dueDate', 'medium', false);
+manager.getList(listId).addTodo('Feed Dog #2', 'Feed dog some food', 'dueDate', 'medium', false);
+console.log(manager.getList(listId));
+manager.getList(listId).removeTodo(1);
+console.log(manager.getList(listId));
