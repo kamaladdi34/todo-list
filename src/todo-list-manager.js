7f381f3a4a51b694130f108fafc13f3ff2277eb7
monmac <monmac@MacBook-Pro-de-monmac.local>
2023-09-13 17:01:37 +0100

    Fix issue where new projects are not saved

--- src/todo-list-manager.js
+++ src/todo-list-manager.js
@@ -1,9 +1,11 @@
 import List from "./list";
+import storageManager from "./storage-manager";
 const manager = (()=>{
     let lists = [];
     const removeList = (id)=>{
         let index = lists.map((e)=> e.id).indexOf(id);
         lists.splice(index,1);
+        storageManager.storeProjects();
     } 
     const getCurrentLists = () => lists;
     const getListIndexById = (id)=>{
@@ -17,6 +19,7 @@ const manager = (()=>{
         let list = new List(name, description, todos, isPersistent);
         list.id = lists.length;
         lists.push(list);
+        storageManager.storeProjects();
         return {id: list.id, project: list};
     }
     return {createList, removeList, getCurrentLists, getList};
